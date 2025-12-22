c1cb6be5abf5cf7f0f0fd5bdb7f07e2a6abcb670
Sadiul Hakim <sadiulhakim@gmail.com>
2025-12-22 20:23:31 +0600

    Day 1

---
+++ dataType.php
@@ -0,0 +1,25 @@
+<?php
+    $string="This is string data type.";
+    $integer = 123;
+    $float=1.12;
+    $boolean=false;
+    $null_value=null;
+
+    $array_example = [1,2,3,4,5];
+    $mixed_array_example = [1,2,3,4,5,true,"Hi"];
+
+    // The
+
+    // $dog = new Animal();
+
+    // $connection = ftp_connect("127.0.0.1") or die("Localhost not found");
+
+    echo var_dump($string)."<br/>";
+    echo var_dump($integer)."<br/>";
+    echo var_dump($float)."<br/>";
+    echo var_dump($boolean)."<br/>";
+    echo var_dump($null_value)."<br/>";
+    echo var_dump($array_example)."<br/>";
+    echo var_dump($mixed_array_example)."<br/>";
+    // echo var_dump($connection)."<br/>";
+?>
---
+++ operators.php
@@ -0,0 +1,13 @@
+<?php
+     
+    // 1. Arithmetic operators
+    // +, -, *, /, %, **
+    echo 10 ** 2; // Power
+    // 2. Assignment operators
+    // 3. Comparison operators
+    // 4. Increment/Decrement operator
+    // 5. Logical operators
+    // 6. String operators
+    // 7. Array operators
+    // 8. Assignment operators
+?>
